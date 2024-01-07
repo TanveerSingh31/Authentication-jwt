bb396f04389cd3bc41b3778e5fc8ae0230acc9bf
TanveerSingh31 <tanveersinghnayyar@gmail.com>
2024-01-07 15:08:48 +0530

    using react-spinner package for loading..

--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import {SyncLoader} from "react-spinners";
 
+const override =  {
+    display: "block",
+    margin: "40vh 45vw",
+  };
 
 export default function Loading(){
-    return (<h2> Loading ...</h2>);
+    return (
+
+        <div className="sweet-loading">
+          <SyncLoader
+            color={"#f5ba13"}
+            cssOverride={override}
+            size={20}
+            aria-label="Loading Spinner"
+          />
+        </div>
+      );
 }
 
