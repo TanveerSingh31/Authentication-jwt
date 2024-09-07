12b6f5a7a91f596af07b530a61d8c0b0cae8ff12
Tanveer Singh <tanveersingh@Tanveers-MacBook-Air.local>
2024-09-07 12:58:37 +0530

    check3

--- Backend/src/Route/routes.js
+++ Backend/src/Route/routes.js
@@ -2,14 +2,8 @@ import express from 'express';
 const Route = express.Router();
 import * as UserController from '../controllers/registerUser.js';
 
-
-
-
 Route.post("/register", UserController.registerUser);
 Route.post("/login", UserController.loginUser);
 Route.get("/userProfile", UserController.getUserProfileByUserId)
-
-
-
 
 export default Route;
